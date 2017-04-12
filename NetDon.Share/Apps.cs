a6e25deb2ec087ffedd352ebc7b04c7fe9caf5a6
Chii Ayano <ayano@l.udonge.net>
2017-04-12 20:46:04 +0900

    commit

---
+++ NetDon.Share/Apps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDon
+{
+    public class Apps
+    {
+        [Flags]
+        public enum Scope
+        {
+            Read =      0x00,
+            Write =     0x01,
+            Follow =    0x10
+        }
+
+        public static async Task RegisterAppAsync(string mastdonUri, string clientName, string redirectUri, Scope scopes, string webSite)
+        {
+
+        }
+    }
+}
